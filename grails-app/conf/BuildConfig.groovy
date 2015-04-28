62a0384c8f837e520ecaa452edb1a2c878b3fa96
Antonio de la Torre <antonio.delatorre@kaleidos.net>
2015-04-28 23:45:42 +0200

    version 1.0.2 - change dependencies compile to build and upgrade plugin versions

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,11 +14,11 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        compile 'net.kaleidos:urbanairship-client:0.3.2'
+        runtime 'net.kaleidos:urbanairship-client:0.3.2'
     }
 
     plugins {
-        build ':release:3.0.1', ':rest-client-builder:2.0.3', {
+        build ':release:3.1.1', ':rest-client-builder:2.1.1', {
             export = false
         }
     }
